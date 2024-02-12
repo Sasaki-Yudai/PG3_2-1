597ac862e97a8d2278e6bcd37abeca4190c13929
Sasaki-Yudai <115062954+Sasaki-Yudai@users.noreply.github.com>
2024-02-12 10:40:39 +0900

    06_02

--- main.cpp
+++ main.cpp
@@ -1,21 +1,45 @@
 #include <stdio.h>
-#include "Enemy.h"
+#include <list>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <thread>
+
+using namespace std;
+
+void Add(int num)
+{
+	num += 1;
+
+	printf("thread %d\n", num);
+};
+
+void Add2(int num)
+{
+	num += 2;
+
+	printf("thread %d\n", num);
+};
+
+void Add3(int num)
+{
+	num += 3;
+
+	printf("thread %d\n", num);
+};
+
 
 int main()
 {
+	int num = 0;
 
-	Enemy enemy_;
+	std::thread th1(Add, num);
+	std::thread th2(Add2, num);
+	std::thread th3(Add3, num);
 
-	bool GameLoop = true;
-
-	int i = 0;
-
-	while (i < 3)
-	{
-		enemy_.Update();
-		i++;
-	}
-
+	th1.join();
+	th2.join();
+	th3.join();
 
 	return 0;
 }
